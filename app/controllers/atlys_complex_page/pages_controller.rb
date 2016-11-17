7a9404451cdefee954f815d127f5a491d770e3f0
hunterlong <info@socialeck.com>
2016-11-16 22:39:35 -0700

    updates

--- app/controllers/atlys_complex_page/pages_controller.rb
+++ app/controllers/atlys_complex_page/pages_controller.rb
@@ -28,7 +28,7 @@ module AtlysComplexPage
       @layout = { "layouts": [{ "origin": "html", "inner": [{ "html": URI.encode(params["html"]), "css": URI.encode(params["css"]), "javascript": URI.encode(params["javascript"]) }] }] }
 
       @page = Page.new
-      @page.json_layout = @layout
+      @page.json_layout = @layout.to_json
       @page.title = params["title"]
       @page.page_url = params["page_url"]
       @page.fromuser = current_user.id
