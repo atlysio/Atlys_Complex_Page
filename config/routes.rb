9aad2de7e441bca08476fa1c5ea99a3deebb5c36
hunterlong <info@socialeck.com>
2016-11-16 21:24:26 -0700

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 AtlysComplexPage::Engine.routes.draw do
-  resources :pages
+  get 'admin/complex_page', to: 'pages#index', as: 'complex_pages'
+  get 'admin/complex_page/new', to: 'pages#new', as: 'new_complex_page'
+  get 'admin/complex_page/edit/:id', to: 'pages#edit', as: 'edit_complex_page'
+  post 'admin/complex_page/create', to: 'pages#create', as: 'create_complex_page'
+  post 'admin/complex_page/update', to: 'pages#update', as: 'update_complex_page'
 end
